f5b6a01abf8ff0eaf051eaf558a119d248639c28
timmy <eriksoehnel@googlemail.com>
2012-06-09 20:01:59 +0200

    Make sure to always close requests.
    Send an HTTP status 404 for missing terminal process responses after a
    30 second timeout.
    Use a common function to create HTTP 404 responses. Ensure that connections
    are always closed.

--- schirm/webserver.py
+++ schirm/webserver.py
@@ -61,11 +61,20 @@ class Server(object):
     delivered.
     """
 
+    # close the connections for requests which have been dispatched to the terminal
+    # and received no response after this amount of seconds
+    REQUEST_TIMEOUT = 30
+
     def __init__(self, pty, user_css='user.css'):
         self.pty = pty
         self.socket = socket.socket()
+        # the terminal process will receive the request data and a
+        # connection-id and its response must contain the
+        # connection-id to determine which connection to use for
+        # sending the response
         self.requests = {}
-        self._id = 0
+        self._requests_lock = threading.RLock() # aquire before touching self.requests
+        self._id = 0 # last request id
         self.resources = {}
 
         # default static resources:
@@ -89,11 +98,15 @@ class Server(object):
 
     def start(self):
         backlog = 5
-        # todo: multithreading (fixed size threadpool (2-3 threads))
         self.socket.bind(('localhost',0))
         self.socket.listen(backlog)
+        # listen to connections and write things to the terminal process
         self.listen_thread = threading.Thread(target=self.listen)
         self.listen_thread.start()
+        # close the connections for requests the terminal process has
+        # not responded in REQUEST_TIMEOUT time
+        self.prune_old_request_thread = threading.Thread(target=self.prune_old_requests)
+        self.prune_old_request_thread.start()
         return self
 
     def getport(self):
@@ -107,9 +120,27 @@ class Server(object):
             client, address = self.socket.accept()
             self.receive(client)
 
+    def prune_old_requests(self):
+        while True:
+            with self._requests_lock:
+                current_time = time.time()
+                for rid in self.requests.keys():
+                    req = self.requests.get(rid)
+                    t = req['time']
+                    if t and current_time - t > self.REQUEST_TIMEOUT:
+                        req = self.requests.pop(rid, None)
+                        req['client'].sendall(self.make_status404())
+                        self._close_conn(req['client'])
+                        logging.info("request {rid} timed out".format(rid=rid))
+            time.sleep(1)
+
     def clear_resources(self):
         #self.resources = {}
         pass
+
+    @staticmethod
+    def _close_conn(client):
+        client._sock.close()
 
     def receive(self, client):
         """
@@ -118,7 +149,9 @@ class Server(object):
         """
         rfile = client.makefile()
         req = HTTPRequest(rfile)
-        logging.info("request: {r.command} {r.path}".format(r=req))
+
+        logging.info("request: {command} {path}".format(command=getattr(req, 'command', None),
+                                                        path=getattr(req, 'path', None)))
         (scheme, netloc, path, params, query, fragment) = urlparse.urlparse(req.path)
         m = re.match("(.+)\.localhost", netloc)
         if m:
@@ -129,7 +162,7 @@ class Server(object):
         if req.error_code:
             logging.debug(req.error_message)
             client.sendall(req.error_message)
-            client.close()
+            self._close_conn(client)
 
         elif req.command == 'GET' \
                 and iframe_id \
@@ -138,7 +171,7 @@ class Server(object):
             # its a known static resource, serve it!
             logging.debug("serving static resource {} for iframe {}".format(path, iframe_id))
             client.sendall(self.resources[iframe_id][path])
-            client.close()
+            self._close_conn(client)
 
         elif req.command == 'GET' \
                 and path in self.schirm_resources:
@@ -150,33 +183,35 @@ class Server(object):
                 try:
                     with open(res, 'r') as f:
                         data = f.read()
+                    logging.debug("serving builtin static resource {} from external path {}.".format(path, res))
                     client.sendall(self.make_response(self.guess_type(path), data))
                 except:
                     logging.error("failed to load static resource {} from path {}.".format(path, res))
-                    client.sendall("HTTP/1.1 404 Not Found")
+                    client.sendall(self.make_status404())
             else:
                 # internal resource
                 logging.debug("serving builtin static resource {}.".format(path))
                 data = pkg_resources.resource_string('schirm.resources', res)
                 client.sendall(self.make_response(self.guess_type(path), data))
 
-            client.close()
+            self._close_conn(client)
 
         elif iframe_id and req.command == 'GET' and path in self.not_found:
             logging.debug("Ignoring request ({})".format(path))
             # ignore some requests (e.g. favicon)
-            client.sendall("HTTP/1.1 404 Not Found")
+            client.sendall(self.make_status404())
+            self._close_conn(client)
 
-        elif not self.pty.screen.iframe_mode:
-            logging.debug("Not in iframe mode - responding with 404")
-            # only serve non-static requests if terminal is in iframe mode
-            client.sendall("HTTP/1.1 404 Not Found")
-            client.close()
+        elif self.pty.screen.iframe_mode == 'closed':
+            # Write requests into stdin of the current terminal process.
+            # Only if all document data has already been sent to the iframe.
+            # So that requests are not echoed into the document or
+            # into the terminal screen.
 
-        else:
-            req_id = self._getnextid()
-            self.requests[req_id] = client
-            
+            with self._requests_lock:
+                req_id = self._getnextid()
+                self.requests[req_id] = {'client': client, 'time': time.time()}
+
             def clear_path(path):
                 # transform the iframe path from a proxy path to a normal one
                 root = "http://{}.localhost".format(iframe_id)
@@ -201,33 +236,42 @@ class Server(object):
                 data.append("")
 
             pty_request = START_REQ + SEP.join(base64.encodestring(x) for x in data) + END + NEWLINE
-
-            # Do only send requests when the terminals iframe document
-            # is 'closed' so that requests are not echoed into the
-            # document or into the terminal screen.
-            # Wait for a close if its currently 'open'
-            timeout = 120
-            wait = 0.1
-            while self.pty.screen.iframe_mode == 'open' \
-                    and timeout > 0:
-                timeout -= wait
-                time.sleep(wait)
-
             if self.pty.screen.iframe_mode == 'closed':
                 self.pty.q_write_iframe(pty_request)
 
+        else:
+            # only serve non-static requests if terminal is in iframe_mode == 'open'
+            if self.pty.screen.iframe_mode == 'open':
+                logging.debug("unknown resource: '{}' - responding with 404".format(path))
+            else:
+                logging.debug("Not in iframe mode - responding with 404")
+
+            client.sendall(self.make_status404())
+            self._close_conn(client)
+
     def respond(self, req_id, data):
         logging.debug("server responding to {} with {}...".format(req_id, data[:60]))
-        rid = int(req_id)
-        if rid in self.requests:
-            client = self.requests[rid]
+        with self._requests_lock:
+            req = self.requests.pop(int(req_id), None) # atomic
+        if req:
+            client = req
             client.sendall(data)
-            client.close()
+            self._close_conn(client)
+
+    def make_status404(self):
+        data = "not found"
+        return '\r\n'.join(["HTTP/1.1 404 Not Found",
+                            "Content-Type: text/plain",
+                            "Content-Length: " + str(len(data)),
+                            "Connection: close",
+                            "",
+                            data])
 
     def make_response(self, mimetype, data):
         """Return a string making up an HTML response."""
         return "\n".join(["HTTP/1.1 200 OK",
                           "Cache-Control: " + "no-cache",
+                          "Connection: close",
                           "Content-Type: " + mimetype,
                           "Content-Length: " + str(len(data)),
                           "",
