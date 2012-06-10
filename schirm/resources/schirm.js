0eed199d6a113a8554bdc5c938b796d34504d132
timmy <eriksoehnel@googlemail.com>
2012-06-10 08:19:35 +0200

    Add iframe resize functions to schirm.js.

--- schirm/resources/schirm.js
+++ schirm/resources/schirm.js
@@ -2,9 +2,60 @@
 // schirm module
 
 var schirm = (function(schirm) {
-  
-  this.resize = function(height) {
-    console.log("iframeresize"+height);
+
+  // Determine and cache the height of a vertical scrollbar
+  var __vscrollbarheight;
+  var getVScrollbarHeight = function() {
+    var compute = function() {
+      var div = document.createElement("div");
+      div.style.width = 100;
+      div.style.height = 100;
+      div.style.overflowX = "scroll";
+      div.style.overflowY = "scroll";
+
+      var content = document.createElement("div");
+      content.style.width = 200;
+      content.style.height = 200;
+
+      div.appendChild(content);
+      document.body.appendChild(div);
+
+      var height = 100 - div.clientHeight;
+
+      document.body.removeChild(div);
+
+      if (height > 0) {
+        return height;
+      } else {
+        return 0;
+      }
+    };
+
+    if (__vscrollbarheight === undefined) {
+      __vscrollbarheight = compute();
+    }
+    return __vscrollbarheight;
+  }
+
+  // return true if el will render with a vertical scrollbar
+  var vScrollbarRequired = function(el) {
+    return el.scrollWidth > el.clientWidth;
+  }
+
+  schirm.resize = function(height) {
+    var bodyStyle = getComputedStyle(document.body);
+    var bodyMargin = parseInt(bodyStyle.marginTop) + parseInt(bodyStyle.marginBottom);
+
+    var vScrollbarHeight = 0;
+    if (vScrollbarRequired(document.body)) {
+      vScrollbarHeight = getVScrollbarHeight()
+    }
+    console.log(height);
+    console.log(bodyMargin);
+    console.log(vScrollbarHeight);
+    var newHeight = height + bodyMargin + vScrollbarHeight;
+
+    console.log("iframeresize"+newHeight);
   }
 
   return schirm;
