ea94b4da058c745632b27a2f6b371eece1186dbb
timmy <eriksoehnel@googlemail.com>
2012-12-12 07:27:54 +0100

    Launch one of chrome or chromium, properly kill the browser on exit.

--- schirm/termserver.py
+++ schirm/termserver.py
@@ -1,19 +1,43 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import subprocess
+import signal
 
 import schirm
 import webserver
 
 def start_chromium(proxy_port):
-    cmd = ' '.join(('chromium-browser',
-                    '--temp-profile', # temp-profile must come first!
-                    '--proxy-server="localhost:%s"' % proxy_port,
-                    '--app="http://termframe.localhost/term.html"',
-                    ))
-    return subprocess.Popen(cmd, shell=True)
+
+    def is_available(cmd):
+        try:
+            subprocess.check_output([cmd, '-version'])
+            return True
+        except OSError:
+            return False
+
+    commands = ['google-chrome', 'chromium-browser']
+    available = filter(is_available, commands)
+
+    if available:
+        cmd = available[0]
+        args = ['--temp-profile', # temp-profile must come first!
+                '--proxy-server=localhost:%s' % proxy_port,
+                '--app=http://termframe.localhost/term.html']
+        p = subprocess.Popen([cmd] + args)
+        # use a process group to kill all the browsers processes at
+        # once on exit
+        os.setpgid(p.pid, os.getpid())
+        return p
+    else:
+        raise Exception("No suitable browser found!")
 
 class UIProxy(object):
+
+    def __init__(self, quit_cb=None):
+        self.quit_cb = quit_cb
+
     def execute_script(self, src):
         print "execute script", repr(src)
 
@@ -31,14 +55,20 @@ class UIProxy(object):
         print "close"
 
     def quit(self):
-        print "QUIT"
+        if self.quit_cb:
+            self.quit_cb()
 
 # TODO: sort this out
 ws = None
 
 def start():
     global ws
-    s = schirm.Schirm(UIProxy(), websocket_proxy_hack=False)
+
+    p = None
+    def _quit():
+        os.killpg(os.getpid(), signal.SIGTERM)
+
+    s = schirm.Schirm(UIProxy(_quit), websocket_proxy_hack=False)
     ws = webserver.Server(s)
     p = start_chromium(ws.getport())
     p.wait()
