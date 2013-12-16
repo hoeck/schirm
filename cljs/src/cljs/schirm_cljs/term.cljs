cedfb798bf2d6d854fee85fb7c3f346283ab50b0
hoeck <eriksoehnel@googlemail.com>
2013-12-16 06:26:27 +0100

    Use the correct style arguments and clean up the file.

--- cljs/src/cljs/schirm_cljs/term.cljs
+++ cljs/src/cljs/schirm_cljs/term.cljs
@@ -7,12 +7,6 @@
 
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
-;; (.write js/document "Hello, ClojureScript!")
-;;
-;; (defn test-fn [x]
-;;   (print "x is" x))
-
-
 ;; events -> chan
 ;; socket-messages -> chan
 
@@ -24,15 +18,12 @@
                             (screen/adjust screen))
       "reset"  (screen/reset screen (nth args 0))
       "resize" (screen/set-size (nth args 0))
-      "insert-overwrite" (let [[line, col, string, attrs] args]
+      "insert-overwrite" (let [[line, col, string, attrs] args
+                               style (apply screen/->CharacterStyle attrs)
+                               ss (screen/StyledString. string style)]
                            (screen/update-line screen
                                                line
-                                               #(screen/line-insert-overwrite
-                                                 %
-                                                 (screen/StyledString. string
-                                                                       (apply screen/->CharacterStyle args))
-                                                 col
-                                                 ))))))
+                                               #(screen/line-insert-overwrite % ss col))))))
 
 (defn setup-screen [parent-element input-chan]
   (let [screen (screen/create-scrollback-screen parent-element)]
@@ -56,18 +47,12 @@
          (.send ws msg)
          (recur))))))
 
-(defn setup-terminal
-  []
+(defn setup-terminal []
   (let [ws-send  (chan)
         ws-recv (chan)
         ws-url (format "ws://%s" (-> js/window .-location .-host))]
     (setup-screen (dom-utils/select 'body) ws-recv)
-    (setup-websocket ws-url ws-send ws-recv)
-    ;; (go (loop []
-    ;;       (let [msg (<! ws-recv)]
-    ;;         (.log js/console "-- recv via chan:" msg)
-    ;;         (recur))))
-    ))
+    (setup-websocket ws-url ws-send ws-recv)))
 
 (defn init []
   (.addEventListener js/document "readystatechange"
